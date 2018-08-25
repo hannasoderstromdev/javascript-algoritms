9224220488ba1d26669ded442fef9b37d7fc83cc
Hanna Söderström <gothbarbie84@gmail.com>
2018-08-25 14:48:01 +0200

    add mergeSort and update README

--- README.md
+++ README.md
@@ -2,10 +2,41 @@
 
 Snippets for a bunch of useful JavaScript algorithms.
 
-## naiveSearch()
+## Beginner
 
-Compares a string with another string and returns number of matches found.
+| naiveSearch()  |                                                                            |
+| -------------- | -------------------------------------------------------------------------- |
+| _How it works_ | Compares a string with another string and returns number of matches found. |
+| _Positive_     | Simple, good for small data sets.                                          |
+| _Negative_     | Inefficient for large data sets.                                           |
+| _Efficiency_   | O(n^2)                                                                     |
 
-## bubbleSort()
+| bubbleSort()   |                                             |
+| -------------- | ------------------------------------------- |
+| _How it works_ | Swapping, sorting from smallest to largest. |
+| _Positive_     | Simple, good for small data sets.           |
+| _Negative_     | Inefficient for large data sets.            |
+| _Efficiency_   | O(n^2)                                      |
 
-Sorts an array of numbers from smallest to largest.
+| selectionSort() |                                             |
+| --------------- | ------------------------------------------- |
+| _How it works_  | Swapping, sorting from largest to smallest. |
+| _Positive_      | Simple, good for small data sets.           |
+| _Negative_      | Inefficient for large data sets.            |
+| _Efficiency_    | O(n^2)                                      |
+
+| insertionSort() |                                                                                                   |
+| --------------- | ------------------------------------------------------------------------------------------------- |
+| _How it works_  | Sorting into new array.                                                                           |
+| _Positive_      | Simple, good for small data sets and nearly sorted data, or when new data is addded continuously. |
+| _Negative_      | Inefficient for large data sets.                                                                  |
+| _Efficiency_    | O(n^2)                                                                                            |
+
+## Intermediate
+
+| mergeSort()    |                                                                                                                                   |
+| -------------- | --------------------------------------------------------------------------------------------------------------------------------- |
+| _How it works_ | Splits arrays until each array has only one item (= is sorted). Then merge and sort each array until there's only one array left. |
+| _Positive_     | More complex, good for large data sets                                                                                            |
+| _Negative_     |                                                                                                                                   |
+| _Efficiency_   | O(n log n)                                                                                                                        |
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// Naive String Search
+/*
+  Naive String Search
+*/
 function naiveSearch(string, find) {
-  var count = 0
-  for (var i = 0; i < string.length; i++) {
-    for (var j = 0; j < find.length; j++) {
+  let count = 0
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < find.length; j++) {
       console.log(find[j], string[i + j])
       if (find[j] !== string[i + j]) {
         console.log('BREAK')
@@ -16,10 +18,17 @@ function naiveSearch(string, find) {
   }
   return count
 }
-
 //naiveSearch('lorie loled', 'lo')
 
-// Bubble sort with swapping (tries to sort every item)
+// 1. EASIER SORTING ALGORITHMS
+
+/* 
+  Bubble Sort 
+    Swapping, from smallest to biggest
+  + Simple, good for small data sets
+  - Not very efficient 
+    Time Complexity Average: O(n^2)
+*/
 function bubbleSort(arr) {
   let arrayIsSorted
   for (let i = arr.length; i > 0; i--) {
@@ -39,6 +48,110 @@ function bubbleSort(arr) {
   }
   return arr
 }
-
 // bubbleSort([37, 45, 29, 8, 12, 88, -3])
-bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])
+// bubbleSort([8, 1, 2, 3, 4, 5, 6, 7])
+
+/* 
+  Selection Sort 
+    Swapping, from biggest to smallest
+  + Simple, good for small data sets
+  - Not good for nearly sorted lists
+    Time Complexity Average: O(n^2)
+ */
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j
+      }
+    }
+    if (i !== lowest) {
+      console.log('before', arr)
+      const temp = arr[i]
+      arr[i] = arr[lowest]
+      arr[lowest] = temp
+      console.log('after', arr)
+    }
+  }
+  return arr
+}
+//selectionSort([34, 22, 10, 19, 17])
+
+/* 
+  Insertion Sort
+  - Complex
+  + Good for nearly sorted arrays or where new data is continuously added
+    Time Complexity Average: O(n^2)
+*/
+function insertionSort(arr) {
+  for (var i = 1; i < arr.length; i++) {
+    const current = arr[i]
+    for (var j = i - 1; j >= 0 && arr[j] > current; j--) {
+      arr[j + 1] = arr[j]
+      console.log(arr[j])
+    }
+    arr[j + 1] = current
+    console.log(arr)
+  }
+  return arr
+}
+//insertionSort([2, 1, 9, 76, 4])
+
+// 2. INTERMEDIATE SORTING ALGORITHMS
+
+/* 
+  Merge part of mergeSort
+*/
+function merge(arr1, arr2) {
+  console.log('merge!')
+  const results = []
+  let i = 0
+  let j = 0
+
+  // Neither array is sorted
+  while (arr1.length > i && arr2.length > j) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i])
+      i++
+    } else {
+      results.push(arr2[j])
+      j++
+    }
+    console.log(results, 'sorting both...')
+  }
+
+  // Second array is sorted, continue with first
+  while (i < arr1.length) {
+    results.push(arr1[i])
+    i++
+    console.log(results, 'sorting first...')
+  }
+
+  // First array is sorted, continue with second
+  while (j < arr2.length) {
+    results.push(arr2[j])
+    j++
+    console.log(results, 'sorting second...')
+  }
+  console.log(results, 'finished!')
+  return results
+}
+// merge([1, 10, 50], [2, 14, 99, 100])
+
+/* 
+  Merge Sort
+  recursive function
+  Time Complexity Average: O(n log n)
+*/
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr
+
+  let mid = Math.floor(arr.length / 2)
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+
+  return merge(left, right)
+}
+
+mergeSort([10, 24, 76, 73])
