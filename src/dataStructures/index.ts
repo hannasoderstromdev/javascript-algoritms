e89d218b696cafc19af596430be128e221569c4b
Hanna Söderström <hanna.soderstrom@frisq.se>
2021-04-10 15:32:23 +0200

    feat: add Stack and Queue

---
+++ src/dataStructures/index.ts
@@ -0,0 +1,13 @@
+import SinglyLinkedList from "./SinglyLinkedList";
+import DoublyLinkedList from "./DoublyLinkedList";
+import Stack from "./Stack";
+import Queue from "./Queue";
+
+const dataStructures = {
+  SinglyLinkedList,
+  DoublyLinkedList,
+  Stack,
+  Queue,
+};
+
+export default dataStructures;
